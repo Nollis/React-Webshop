3cbeaa21275149a6ded963c585f5bbd1bf37325f
Niklas Bergh <nollis@icloud.com>
2023-02-01 07:44:01 +0100

    ons

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -12,6 +12,20 @@ const Login = () => {
     const [cookies, setCookie] = useCookies(['UserId']);
 
     const handleFormSubmit = (values) => {
+
+        // fetch(`https://localhost:7127/api/Candy/login`, {
+        //     method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        //     mode: 'cors', // no-cors, *cors, same-origin
+        //     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: 'include', // include, *same-origin, omit
+        //     headers: {
+        //     'Content-Type': 'application/json'
+        //     // 'Content-Type': 'application/x-www-form-urlencoded',
+        //     },
+        //     redirect: 'follow', // manual, *follow, error
+        //     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        //     body: JSON.stringify(values) // body data type must match "Content-Type" header
+        // });
         
         api(`/api/Candy/login`, {
             method: "post",
@@ -31,7 +45,10 @@ const Login = () => {
         .catch(function (res) {
           console.log(res);
         });
-        api.get(`/api/Candy/checkadmin`).then((result) => {
+
+        api(`/api/Candy/checkadmin`, {
+            method: "get",
+            withCredentials: false}).then((result) => {
             console.log(result.data);
             
             if (result.data !== '') {
